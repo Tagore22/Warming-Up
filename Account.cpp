766363587aee0952bc872f3d8ea5e74a6416fcac
Myers <67143011+Tagore22@users.noreply.github.com>
2020-06-20 18:18:14 +0900

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -1,50 +1,37 @@
 #include <Account.h>
 #include <iostream>
 #include <cstring>
-
-/*private:
- int ID;
- int Money;
- char * Name;
-public:
- Bank(int a, int b, char * c);
- ~Bank();
- Virtual void SetMoney(int a);
- int GetMoney() const;
- void GetID() const;
- Virtual void ShowAccount() const;
-};*/
  
 
 using namespace std;
 
-Bank::Bank(int a, int b, char * c) : ID(a), Money(b)
+Account::Account(int a, int b, char * c) : ID(a), Money(b)
 {
   Name = new char[strlen(c)+1];
   strcpy(Name, c);
 }
 
-Bank::~Bank()
+Account::~Account()
 {
  delete []Name;
 }
 
-void Bank::SetMoney(int a)
+void Account::SetMoney(int a)
 {
  Money += a;
 }
 
-int Bank::GetMoney() const
+int Account::GetMoney() const
 {
  return Money;
 }
 
-void Bank::GetAccount() const
+void Account::GetID() const
 {
  return ID;
 }
 
-void Bank::ShowBank() const
+void Account::ShowAccount() const
 {
  cout<<"계좌번호 : "<<ID<<endl;
  cout<<"잔 액 : "<<Money<<endl;
