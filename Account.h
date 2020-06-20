049f5865b95cb2be0052f6f5eac8b3e0b093d878
Myers <67143011+Tagore22@users.noreply.github.com>
2020-06-20 18:17:25 +0900

    Update Account.h

--- Account.h
+++ Account.h
@@ -16,39 +16,7 @@ public:
  Virtual void ShowAccount() const;
 };
  
- 
-Account::Account(int a, int b, char * c) : ID(a), Money(b)
-{
-  Name = new char[strlen(c)+1];
-  strcpy(Name, c);
-}
 
-Account::~Account()
-{
- delete []Name;
-}
-
-void Account::SetMoney(int a)
-{
- Money += a;
-}
-
-int Account::GetMoney() const
-{
- return Money;
-}
-
-void Account::GetID() const
-{
- return ID;
-}
-
-void Account::ShowAccount() const
-{
- cout<<"계좌번호 : "<<ID<<endl;
- cout<<"잔 액 : "<<Money<<endl;
- cout<<"예금주 : "<<Name<<endl;
-}
  
 #endif
  
