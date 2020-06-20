b573714b63a194b9643efa0ae58309cccc626303
Myers <67143011+Tagore22@users.noreply.github.com>
2020-06-20 18:33:39 +0900

    Update Account.h

--- Account.h
+++ Account.h
@@ -12,7 +12,6 @@ public:
  ~Account();
  Virtual void Deposit(int a); //입금 함수. 계좌의 신용도마다 이자가 다르기에 입금시에 추가 이자를 함께 입금하여 구현.
  int WithDraw(int a); //출금 함수.
- int GetMoney() const;
  void GetID() const;
  Virtual void ShowAccount() const;
 };
