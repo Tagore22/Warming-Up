9e0c94f877bf3ce1f35c0ea7c95709c8bf6d2b09
Myers <67143011+Tagore22@users.noreply.github.com>
2020-06-20 16:38:43 +0900

    Create Bank.cpp

---
+++ Bank.cpp
@@ -0,0 +1,36 @@
+#include <Bank.h>
+#include <iostream>
+#include <cstring>
+
+Bank::Bank(int a, int b, char * c) : ID(a), Money(b)
+{
+  Name = new char[strlen(c)+1];
+  strcpy(Name, c);
+}
+
+Bank::~Bank()
+{
+ delete []Name;
+}
+
+void Bank::SetMoney(int a)
+{
+ Money += a;
+}
+
+int Bank::GetMoney() const
+{
+ return Money;
+}
+
+void Bank::GetAccount() const
+{
+ return ID;
+}
+
+void Bank::ShowBank() const
+{
+ cout<<"계좌번호 : "<<ID<<endl;
+ cout<<"잔 액 : "<<Money<<endl;
+ cout<<"예금주 : "<<Name<<endl;
+}
