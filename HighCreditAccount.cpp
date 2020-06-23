ec33d8534e2f0cc5141ee6bb69295624f83e6802
Myers <67143011+Tagore22@users.noreply.github.com>
2020-06-23 18:05:32 +0900

    Create HighCreditAccount.cpp

---
+++ HighCreditAccount.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <HighCreditAccount.h>
+
+HighCreditAccount(int a, int b, char * c, int d, int e) : NormalAccount(a,b,c,d), SpecialRate(e)
+{
+}
+ 
+void HighCreditAccount::Deposit(int a)
+{
+ NormalAccount::Deposit(a);
+ Account::Deposit(Money*(SpecialRate/100.0));
+}
+ 
+void HighCreditAccount::ShowAll() const
+{
+ NormalAccount::ShowAll();
+ cout<<"특별이자율 : "<<SpecialRate<<endl;
+}
